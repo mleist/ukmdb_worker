d7b8f0a426999b68d26a15e29e8293d303dd8632
Markus Leist <markus@lei.st>
2016-07-20 17:55:55 +0200

    more rtd-tests

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,9 @@ import ukmdb_worker
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-#extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
-extensions = ['sphinx.ext.*']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode',
+              'sphinxcontrib.plantuml',
+              ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
